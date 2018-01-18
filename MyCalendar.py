27f8c716835d300888babbbde9d013dff13e7bcc
Justin Tian <tkg8w58@kohls.com>
2018-01-17 22:57:04 -0800

    729 MyCalendar

--- MyCalendar.py
+++ MyCalendar.py
@@ -7,38 +7,38 @@ class MyCalendar:
 
             self.books = []
 
-        def book(self, start, end):
-            """
-            :type start: int
-            :type end: int
-            :rtype: bool
-            """
+    def book(self, start, end):
+        """
+        :type start: int
+        :type end: int
+        :rtype: bool
+        """
 
-            if len(self.books) == 0:
-                self.books.append([start, end])
+        if len(self.books) == 0:
+            self.books.append([start, end])
+            return True
+        else:
+            if end <= self.books[0][0]:
+                self.books.insert(0, [start, end])
                 return True
             else:
-                if end <= self.books[0][0]:
-                    self.books.insert(0, [start, end])
+                for i in range(0, len(self.books)-1):
+                    if start <= self.books[i][0] and end > self.books[i][0]:
+                        return False
+                    elif start > self.books[i][0] and start < self.books[i][1]:
+                        return False
+                    elif start >= self.books[i][1] and end <= self.books[i+1][0]:
+                        self.books.insert(i+1, [start, end])
+                        return True
+                            
+                if end <= self.books[-1][0]:
+                    self.books.insert(len(self.books)-2,[start, end])
+                    return True
+                elif start >= self.books[-1][1]:
+                    self.books.append([start, end])
                     return True
                 else:
-                    for i in range(0, len(self.books)-1):
-                        if start <= self.books[i][0] and end > self.books[i][0]:
-                            return False
-                        elif start > self.books[i][0] and start < self.books[i][1]:
-                            return False
-                        elif start >= self.books[i][1] and end < self.books[i+1][0]:
-                            self.books.insert(i+1, [start, end])
-                            return True
-                            
-                    if end <= self.books[-1][0]:
-                        self.books.insert(len(self.books)-2,[start, end])
-                        return True
-                    elif start >= self.books[-1][1]:
-                        self.books.append([start, end])
-                        return True
-                    else:
-                        return False
+                    return False
 
 # Your MyCalendar object will be instantiated and called as such:
 # obj = MyCalendar()
