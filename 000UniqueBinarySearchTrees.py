ba5def0bfb19c14854cda6df01191393ea3422e6
Justin Tian <tkg8w58@kohls.com>
2018-02-05 16:07:02 -0800

    96. Unique Binary Search Trees

---
+++ 000UniqueBinarySearchTrees.py
@@ -0,0 +1,21 @@
+# 96. Unique Binary Search Trees
+
+# Suppose you are given 1,..., n, and you want to generate all binary search trees. 
+# How do you do it? Suppose you put number i on the root, then simply
+# 1. Generate all BST on the left branch by running the same algorithm with 1–(i-1),
+# 2. Generate all BST on the right branch by running the same algorithm with (i+1)–n.
+# 3. Take all combinations of left branch and right branch, and that’s it for i on the root.
+
+class Solution:
+    def numTrees(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+
+        res = [0] * (n+1)
+        res[0] = 1
+        for i in xrange(1, n+1):
+            for j in xrange(i):
+                res[i] += res[j] * res[i-1-j]
+        return res[n]   
