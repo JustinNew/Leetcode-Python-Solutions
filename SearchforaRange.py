7c1f0cfb2ffdb28dfcfdcc3489f46d751c6f6294
Justin Tian <tkg8w58@kohls.com>
2018-02-13 11:06:57 -0800

    34. Search for a Range

--- SearchforaRange.py
+++ SearchforaRange.py
@@ -1,47 +1,57 @@
-class Solution(object):
+# 34. Search for a Range
+
+class Solution:
     def searchRange(self, nums, target):
         """
         :type nums: List[int]
         :type target: int
         :rtype: List[int]
         """
-        result = []
+
         if len(nums) == 0:
-            return [-1,-1]
-        if nums[0] > target or nums[-1] < target:
-            return [-1,-1]
-        
+            return [-1, -1]
+
+        begin = -1
+        end = -1
+
+        # Binary Search for Begin
         low = 0
         high = len(nums) - 1
         while low <= high:
-            mid = low + int((high - low)/2)
+
+            mid = int((high + low)/2)
             if nums[mid] > target:
                 high = mid - 1
             elif nums[mid] < target:
                 low = mid + 1
-            elif nums[mid] == target:
-                if mid == 0 or nums[mid - 1] < target:
-                    result.append(mid)
+            else:
+                if mid == 0:
+                    begin = 0
+                    break
+                elif nums[mid - 1] < target:
+                    begin = mid
                     break
                 else:
                     high = mid - 1
-                
+
+        # Binary Search for End
         low = 0
-        high = len(nums) - 1        
+        high = len(nums) - 1
         while low <= high:
-            mid = low + int((high - low)/2)
+
+            mid = int((high + low)/2)
             if nums[mid] > target:
                 high = mid - 1
             elif nums[mid] < target:
                 low = mid + 1
-            elif nums[mid] == target:
-                if mid == len(nums) - 1 or nums[mid + 1] > target:
-                    result.append(mid)
+            else:
+                if mid == len(nums) - 1:
+                    end = len(nums) - 1
+                    break
+                elif nums[mid + 1] > target:
+                    end = mid
                     break
                 else:
                     low = mid + 1
-                    
-        if len(result) == 0: 
-            return [-1,-1]
-        else:
-            return result
+
+        return [begin, end]
