e4024bf10fccb8b4820f1c5721745532e99ff857
Justin Tian <tkg8w58@kohls.com>
2018-02-13 15:22:17 -0800

    Minor changes

--- 000FindtheDerangementofAnArray.py
+++ 000FindtheDerangementofAnArray.py
@@ -38,22 +38,17 @@ class Solution2:
         :type n: int
         :rtype: int
         """
-
-        mul = 1
-        sum = 0
-        M = 1000000007
-
-        for i in range(n, -1, -1):
-            if i % 2 == 0:
-                temp = 1
-            else: 
-                temp = -1
-            sum = (sum + M + mul * temp) % M
-            mul = (mul * i) % M
-
-        return sum 
+        
+        result = [0 for i in range(n+1)]
+        result[0] = 0
+        result[1] = 0
+        result[2] = 1
+        for i in range(3, n+1):
+            result[i] = (i-1)*(result[i-1] + result[i-2])
+            
+        return result[-1] 
 
 if __name__ == '__main__':
 
-    s = Solution()
+    s = Solution2()
     print (s.findDerangement(10))
