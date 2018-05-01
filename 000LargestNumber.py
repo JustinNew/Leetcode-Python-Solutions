bdbe73a5501da7aee2bf90f32358fed77dbc5c5c
Justin Tian <tkg8w58@kohls.com>
2018-05-01 14:54:33 -0700

    # 179. Largest Number

--- 000LargestNumber.py
+++ 000LargestNumber.py
@@ -1,9 +1,12 @@
 # 179. Largest Number
 
+# If x + y > y + x, then x first followed by y. 
+
 class Solution:
     # @param {integer[]} nums
     # @return {string}
     def largestNumber(self, nums):
+
         nums = [str(x) for x in nums]
         nums.sort(cmp=lambda x, y: cmp(y+x, x+y))
         return ''.join(nums).lstrip('0') or '0'
