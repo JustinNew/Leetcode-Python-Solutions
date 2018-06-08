f6f8143b3462018c72df42de8f397a5c4c898e07
Justin Tian <tkg8w58@kohls.com>
2018-06-08 14:04:35 -0700

    287. Find the Duplicate Number

---
+++ 000FindtheDuplicateNumber.py
@@ -0,0 +1,14 @@
+# 287. Find the Duplicate Number
+
+'''
+The main idea is the same with problem Linked List Cycle II,https://leetcode.com/problems/linked-list-cycle-ii/. Use two pointers the fast and the slow. The fast one goes forward two steps each time, while the slow one goes only step each time. They must meet the same item when slow==fast. In fact, they meet in a circle, the duplicate number must be the entry point of the circle when visiting the array from nums[0]. Next we just need to find the entry point. We use a point(we can use the fast one before) to visit form begining with one step each time, do the same job to slow. When fast==slow, they meet at the entry point of the circle.
+'''
+
+class Solution:
+    def findDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+
+        
