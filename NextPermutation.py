33413a1a346d953b6ddeac1e376ef8cdaf6c9a1d
Justin Tian <tkg8w58@kohls.com>
2018-07-03 14:35:36 -0700

    31. Next Permutation

--- NextPermutation.py
+++ NextPermutation.py
@@ -50,7 +50,54 @@ class Solution:
         print (nums)
         return
         
+class Solution2:
+
+    def nextPermutation(self, l):
+
+        # Find the first ToExchange.
+        flag = 0
+        for i in range(len(l) - 2, -1, -1):
+            ToExchange = i
+
+            for j in range(i+1, len(l)):
+                if l[j] > l[i]:
+                    flag = 1
+                    Exchange = j
+                    break
+
+            if flag == 1:
+                break
+
+        if flag == 0:
+            l = l[::-1]
+            print (l)
+            return
+
+        # Check to get the min() that is larger than l[ToExchange]
+        for i in range(ToExchange + 1, len(l)):
+                if l[i] > l[ToExchange]:
+                    if l[i] < l[Exchange]:
+                        Exchange = i
+
+        # Exchange
+        l[ToExchange], l[Exchange] = l[Exchange], l[ToExchange]
+
+        # Rearrange to get the minimum.
+        l = self.rearrange(l, ToExchange+1, len(l) - 1)
+
+        print (l)
+        return 
+
+    def rearrange(self, l, i, j):
+
+        for m in range(j - i):
+            for n in range(i, len(l) - m - 1):
+                if l[n] > l[n+1]:
+                    l[n], l[n+1] = l[n+1], l[n]
+
+        return l
+
 if __name__ == '__main__':
 
-    s = Solution()
-    s.nextPermutation([3,2,1])
+    s = Solution2()
+    s.nextPermutation([1,2,3])
