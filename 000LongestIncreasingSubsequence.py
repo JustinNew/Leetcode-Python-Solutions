59fb810ea201888721675c47deb07c00d75f3916
Justin Tian <tkg8w58@kohls.com>
2018-07-07 20:55:07 -0700

    Classic: 300. Longest Increasing Subsequence

--- 000LongestIncreasingSubsequence.py
+++ 000LongestIncreasingSubsequence.py
@@ -56,7 +56,33 @@ class Solution:
 
         return max(lis)
 
-if __name__ == '__main__':
+'''
+tails is an array storing the smallest tail of all increasing subsequences with length i+1 in tails[i].
+For example, say we have nums = [4,5,6,3], then all the available increasing subsequences are:
 
-    so = Solution()
-    print(so.lengthOfLIS([1,2,3,4,5,6,7,8,9,10]))
+len = 1   :      [4], [5], [6], [3]   => tails[0] = 3
+len = 2   :      [4, 5], [5, 6]       => tails[1] = 5
+len = 3   :      [4, 5, 6]            => tails[2] = 6
+
+We can easily prove that tails is a increasing array. Therefore it is possible to do a binary search in tails array to find the one needs update.
+
+Each time we only do one of the two:
+
+(1) if x is larger than all tails, append it, increase the size by 1
+(2) if tails[i-1] < x <= tails[i], update tails[i]
+Doing so will maintain the tails invariant. The the final answer is just the size.
+'''
+def lengthOfLIS(self, nums):
+    def search(temp, left, right, target):
+        if left == right:
+            return left
+        mid = left+(right-left)/2
+        return search(temp, mid+1, right, target) if temp[mid]<target else search(temp, left, mid, target)
+    temp = []
+    for num in nums:
+        pos = search(temp, 0, len(temp), num)
+        if pos >=len(temp):
+            temp.append(num)
+        else:
+            temp[pos]=num
+    return len(temp)
