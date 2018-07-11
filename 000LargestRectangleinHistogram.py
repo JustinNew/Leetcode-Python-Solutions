a046118df3c851e29be51f80f90478f57ad36974
Justin Tian <tkg8w58@kohls.com>
2018-07-10 21:00:53 -0700

    classic: 84. Largest Rectangle in Histogram

--- 000LargestRectangleinHistogram.py
+++ 000LargestRectangleinHistogram.py
@@ -26,7 +26,15 @@ class Solution:
         return max(all, left, right)       
 
 '''
-The stack maintain the indexes of buildings with ascending height. Before adding a new building pop the building who is taller than the new one. The building popped out represent the height of a rectangle with the new building as the right boundary and the current stack top as the left boundary. Calculate its area and update ans of maximum area. Boundary is handled using dummy buildings.
+The stack maintain the indexes of buildings with ascending height. 
+Before adding a new building pop the building who is taller than the new one. 
+The building popped out represent the height of a rectangle with the new building 
+as the right boundary and the current stack top as the left boundary. 
+Calculate its area and update ans of maximum area. 
+Boundary is handled using dummy buildings.
+
+1. Using stack to only keep increasing sequence.
+2. When find an element smaller, calculate the rectangle area.
 '''
 
     def largestRectangleArea(self, height):
