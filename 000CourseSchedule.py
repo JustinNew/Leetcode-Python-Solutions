e7a84012c0fa20c01ff832ac39f21f99e963d060
Justin Tian <tkg8w58@kohls.com>
2018-07-12 15:15:16 -0700

    Classic: 207. Course Schedule

--- 000CourseSchedule.py
+++ 000CourseSchedule.py
@@ -28,3 +28,48 @@ class Solution(object):
             if not dfs(i):
                 return False
         return True
+
+# 210. Course Schedule II
+
+class Solution(object):
+    def findOrder(self, numCourses, prerequisites):
+        """
+        :type numCourses: int
+        :type prerequisites: List[List[int]]
+        :rtype: List[int]
+        """
+
+        if len(prerequisites) == 0:
+            return [i for i in range(numCourses)]
+
+        d = {}
+        for i in prerequisites:
+            if i[0] in d:
+                d[i[0]].append(i[1])
+            else:
+                d[i[0]] = [i[1]]
+
+        visited = [-1 for i in range(numCourses)]
+
+        result = []
+        # DFS
+        def dfs(i):
+            if visited[i] == 1:
+                return False
+            elif visited[i] == 0:
+                return True
+            else:
+                visited[i] = 0
+                if i in d:
+                    for j in d[i]:
+                        if dfs(j):
+                            return True
+                visited[i] = 1
+                result.append(i)
+                return False
+
+        for k in range(numCourses):
+            if dfs(k):
+                return []
+
+        return result
