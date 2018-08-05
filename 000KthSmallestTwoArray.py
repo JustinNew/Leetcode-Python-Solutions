e3a7adddf77f50c4df85dd22fbaffe8941a04e57
Justin Tian <tkg8w58@kohls.com>
2018-08-05 14:58:05 -0700

    kth smallest

--- 000KthSmallestTwoArray.py
+++ 000KthSmallestTwoArray.py
@@ -1,15 +1,28 @@
-def kth(self, A, B, k):
-        if len(A) > len(B):
-            A, B = (B, A)
-        if not A:
-            return B[k]
-        if k == len(A) + len(B) - 1:
-            return max(A[-1], B[-1])
-    
-        i = min(len(A)-1, k/2)
-        j = min(len(B)-1, k-i)
-    
-        if A[i] > B[j]:
-            return self.kth(A[:i], B[j:], i)
+# Find the Kth smallest element of two lists.
+
+# Binary search
+# Each time drop half of one array
+
+# kth is '0' based.
+def kth(nums1, nums2, k):
+
+    if not nums1:
+        return nums2[k]
+    elif not nums2:
+        return nums1[k]
+
+    m1 = int(len(nums1) / 2)
+    m2 = int(len(nums2) / 2)
+
+    if m1 + m2 < k:
+        if nums1[m1] < nums2[m2]:
+            return kth(nums1[m1 + 1:], nums2, k - m1 - 1)
         else:
-            return self.kth(A[i:], B[:j], j)
+            return kth(nums1, nums2[m2 + 1:], k - m2 - 1)
+    else:
+        if nums1[m1] < nums2[m2]:
+            return kth(nums1, nums2[:m2], k)
+        else:
+            return kth(nums1[:m1], nums2, k)
+
+print(kth([1,3,5,7], [2,4,6,8], 1))
