576290881111bfe4f5ed1f848dcde35b7342c742
Justin Tian <tkg8w58@kohls.com>
2018-08-12 16:38:21 -0700

    54. Spiral Matrix

--- 000SpiralMatrix.py
+++ 000SpiralMatrix.py
@@ -1,83 +1,126 @@
-# Leetcode 54 Spiral Matrix
-# Extremely Error prone, have to be really careful.
-# Master this can also solve "6 ZigZag Conversion"
+# 54. Spiral Matrix
 
-# Go by circle to print out, except only one row or one column left
+# 1. Layer by layer
+# Go right, down, left and up. Four directions.
+# (i, j) to indicate the location of current element.
+# (m, n) to indicate the steps need to go in each direction.
+# Need to deal with m == 1 or n == 1 special cases.
+# After one layer deal with edge case.
 
-# Take care of case when one row or one column left
-
-# Go by circle: go right, go down, go left, and go up
-# r++, r--, c++, c-- as steps down/up, right/left
-# m, n are the bound. How many steps to go in each direction.
-# After each circle finished, the other dimension reduced by 2.
-# Pay attention to the initial step r = 0, c = 0 and r++ , c++ after each circle.
-# Pay attention to c++/c--, r++/r-- first before print out element.
-
-# Pay attention to m > 0 and n > 0.
-# The corner cases: [], [1,2,3], [[1,2,3]]
-
-class Solution:
+class Solution(object):
     def spiralOrder(self, matrix):
         """
         :type matrix: List[List[int]]
         :rtype: List[int]
         """
-        result = []
         
+        if len(matrix) == 0:
+            return []
+
         m = len(matrix)
-        if m == 0:
-            return []      
-        elif type(matrix[0]) is list: 
-            n = len(matrix[0]) 
-        else:
-            return matrix
-            
-        r = 0
-        c = 0
+        n = len(matrix[0])
 
-        while m > 0 and n > 0:
-            
-            if m == 1:
-                for i in range(n):
-                    result.append(matrix[r][c])
-                    c += 1
-                break
-            elif n == 1:
-                for i in range(m):
-                    result.append(matrix[r][c])
-                    r += 1
-                break
-                    
-            # Process a circle
-            # Go right
-            for i in range(n-1):
-                result.append(matrix[r][c])
-                c += 1
+        i = 0
+        j = 0
+        result = []
+        while m >= 2 and n >= 2:
 
-            # Go down
-            for i in range(m-1):
-                result.append(matrix[r][c])
-                r += 1
+            # Go Right
+            for k in range(n - 1):
+                result.append(matrix[i][j])
+                j += 1
 
-            # Go left
-            for i in range(n-1):
-                result.append(matrix[r][c])
-                c -= 1
-            
-            # Go up
-            for i in range(m-1):
-                result.append(matrix[r][c])
-                r -= 1
-            
-            r += 1
-            c += 1
-            m -= 2    
+            # Go Down
+            for k in range(m - 1):
+                result.append(matrix[i][j])
+                i += 1
+
+            # Go Left
+            for k in range(n - 1):
+                result.append(matrix[i][j])
+                j -= 1
+
+            # Go Up
+            for k in range(m - 1):
+                result.append(matrix[i][j])
+                i -= 1
+
+            # Deal with edge case
+            i += 1
+            j += 1
+
+            m -= 2
             n -= 2
 
+        if m == 1:
+            for k in range(n):
+                result.append(matrix[i][j])
+                j += 1
+        elif n == 1:
+            for k in range(m):
+                result.append(matrix[i][j])
+                i += 1
+
         return result
-           
 
-if __name__ == '__main__':
+# 2. Complete eliminate one row then one column then one row then one column ...
+# # Go right, down, left and up. Four directions.
+# (i, j) to indicate the location of current element.
+# (m, n) to indicate the steps need to go in each direction.
+# There is another kind of edge case when jumped out out loop.
 
-    s = Solution()
-    print (s.spiralOrder([[1],[3],[5]])) 
+    def spiralOrder(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: List[int]
+        """
+        if len(matrix) == 0:
+            return []
+
+        m = len(matrix)
+        n = len(matrix[0])
+
+        i = 0
+        j = -1
+        result = []
+
+        while m >= 2 and n >= 2:
+
+            # Go Right
+            for k in range(n):
+                j += 1 
+                result.append(matrix[i][j])
+            m -= 1
+ 
+            # Go Down
+            for k in range(m):
+                i += 1
+                result.append(matrix[i][j])
+            n -= 1
+
+            # Go Left
+            for k in range(n):
+                j -= 1
+                result.append(matrix[i][j])
+            m -= 1
+
+            # Go Up
+            for k in range(m):
+                i -= 1
+                result.append(matrix[i][j])
+            n -= 1
+
+        if m == 1:
+            # Deal with edge case
+            j += 1
+            for k in range(n):
+                result.append(matrix[i][j])
+                j += 1
+        elif n == 1:
+            # Deal with edge case
+            j += 1
+            for k in range(m):
+                result.append(matrix[i][j])
+                i += 1 
+
+        return result
