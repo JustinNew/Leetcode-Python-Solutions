ce988dbe59f3517fcc4341e1fb09e0924944fe87
Justin Tian <tkg8w58@kohls.com>
2018-08-14 14:22:04 -0700

    89. Grey Code

--- 000GrayCode.py
+++ 000GrayCode.py
@@ -1,25 +1,40 @@
-# 89. Grey Code
+# 89. Gray Code
 
-# Good analysis is needed. 
-# Start from i = 1, 2, 3, .., try to get a solution.
+# Good analysis is needed.
+# First for 1 bit
+#  0
+#  1
+# Then for 2 bit, the first two numbers are actually
+# 00
+# 01
+# Flip the 1 bit numbers and add 1 on the second bit
+# 00     00
+# 01  -> 01
+# 01  -> 11
+# 00     10
+# Then for 3 bit, flip the already have numbers and add 1 in the third bit
+# 000     000
+# 001     001
+# 011     011
+# 010  -> 010
+# 010  -> 110
+# 011     111
+# 001     101
+# 000     100
 
-class Solution:
-    # @return a list of integers
-    '''
-    from up to down, then left to right
-    
-    0   1   11  110
-            10  111
-                101
-                100
-                
-    start:      [0]
-    i = 0:      [0, 1]
-    i = 1:      [0, 1, 3, 2]
-    i = 2:      [0, 1, 3, 2, 6, 7, 5, 4]
-    '''
+class Solution(object):
     def grayCode(self, n):
-        results = [0]
-        for i in range(n):
-            results += [x + pow(2, i) for x in reversed(results)]
-        return results
+        """
+        :type n: int
+        :rtype: List[int]
+        """
+        if n == 0:
+            return [0]
+        elif n == 1:
+            return [0, 1]
+
+        result = [0, 1]
+        for i in range(1, n):
+            result += [j + 2 ** i for j in result[::-1]]
+
+        return result
