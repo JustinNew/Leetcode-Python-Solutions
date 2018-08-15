6acaca16494734f8b0c5150d5251a27d4142d7a0
Justin Tian <tkg8w58@kohls.com>
2018-08-15 13:59:43 -0700

    96. Unique Binary Search Trees

--- 000UniqueBinarySearchTrees.py
+++ 000UniqueBinarySearchTrees.py
@@ -15,7 +15,7 @@ class Solution:
 
         res = [0] * (n+1)
         res[0] = 1
-        for i in xrange(1, n+1):
-            for j in xrange(i):
+        for i in range(1, n+1):
+            for j in range(i):
                 res[i] += res[j] * res[i-1-j]
         return res[n]   
