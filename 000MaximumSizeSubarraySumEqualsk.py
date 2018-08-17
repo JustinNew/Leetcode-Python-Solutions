1ed712cb11025d9853dbc3cbd1f49fdcb3dfdf2b
Justin Tian <tkg8w58@kohls.com>
2018-08-17 14:19:05 -0700

    325. Maximum Size Subarray Sum Equals k

--- 000MaximumSizeSubarraySumEqualsk.py
+++ 000MaximumSizeSubarraySumEqualsk.py
@@ -1,4 +1,10 @@
 # 325. Maximum Size Subarray Sum Equals k
+
+```
+Given an Integer Array, find the maximum size of subarray whose sum equals to K.
+For example, Given [1,2,-3,3,-1,2,4] and K=3, the subarray [1,2,-3,3] sum equals to 3.
+```
+
 # Facebook Tag
 
 # Tricks:
@@ -31,3 +37,23 @@ class Solution(object):
                 sum2pos[tsum] = i + 1
 
         return ans or 0
+
+    def MaxSubArray(self, list, k):
+        d = {}
+        d[0] = -1
+        s = 0
+        result = 0
+        for i in range(len(list)):
+            s += list[i]
+            if s not in d:
+                d[s] = i
+            if k - s in d:
+                t = i - d[k - s]
+                result = max(result, t)
+
+        return result
+
+if __name__ == '__main__':
+
+    so = Solution()
+    print(so.MaxSubArray([1,2,-3,3,-1,2,4], 3))
