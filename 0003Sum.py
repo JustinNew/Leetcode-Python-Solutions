3be9d93be0c15519990c50d627a75d9bfdbc2b6b
Justin Tian <tkg8w58@kohls.com>
2018-08-20 15:07:32 -0700

    15. 3Sum

--- 0003Sum.py
+++ 0003Sum.py
@@ -8,8 +8,11 @@ class Solution(object):
         res = []
         nums.sort()
         for i in xrange(len(nums)-2):
+            # Always do the first duplicates for 3Sum
             if i > 0 and nums[i] == nums[i-1]:
                 continue
+            
+            # 2Sum with duplicates.
             l, r = i+1, len(nums)-1
             while l < r:
                 s = nums[i] + nums[l] + nums[r]
@@ -19,6 +22,7 @@ class Solution(object):
                     r -= 1
                 else:
                     res.append((nums[i], nums[l], nums[r]))
+                    # Skip duplicates
                     while l < r and nums[l] == nums[l+1]:
                         l += 1
                     while l < r and nums[r] == nums[r-1]:
@@ -26,7 +30,23 @@ class Solution(object):
                     l += 1; r -= 1
         return res
 
-if __name__ == '__main__':
-
-    s = Solution()
-    print(s.threeSum([-1,0,1,2,-1,-4]))
+    # Very hard to remove duplicates using dictionary 2Sum approach.
+    def threeSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        if len(nums) < 3:
+            return []
+        nums.sort()
+        res = set()
+        for i, v in enumerate(nums[:-2]):
+            if i >= 1 and v == nums[i-1]:
+                continue
+            d = {}
+            for x in nums[i+1:]:
+                if x not in d:
+                    d[-v-x] = 1
+                else:
+                    res.add((v, -v-x, x))
+        return map(list, res)
