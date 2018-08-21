540d93ae3067500d953f202f32dc0bcef474f343
Justin Tian <tkg8w58@kohls.com>
2018-08-21 14:43:54 -0700

    229. Majority Element II

--- 000MajorityElement.py
+++ 000MajorityElement.py
@@ -71,7 +71,7 @@ class Solution:
             elif count2 == 0:
                 candidate2, count2 = n, 1
             else:
-                count1, count2 = count1 - 1, count2 - 1
+                count1, count2 = count1 - 1, count2 - 1 # Note: both need to subtract 1
 
         return [n for n in (candidate1, candidate2)
                         if nums.count(n) > len(nums) // 3]
