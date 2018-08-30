1115ca70a7a7a975e8869e412d2c703142e72896
Justin Tian <tkg8w58@kohls.com>
2018-08-29 17:31:42 -0700

    121. Best Time to Buy and Sell Stock

---
+++ 000BestTimetoBuyandSellStock.py
@@ -0,0 +1,21 @@
+# 121. Best Time to Buy and Sell Stock
+
+# Greedy
+
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        if len(prices) < 2:
+            return 0
+        
+        low = prices[0]
+        m = 0
+        for i in range(1,len(prices)):
+            m = max(m, prices[i] - low)
+            if prices[i] < low:
+                low = prices[i]
+                
+        return m
