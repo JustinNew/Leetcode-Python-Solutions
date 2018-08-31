69b0cddba40dff6ef2313793b881e0bdb1246ba5
Justin Tian <tkg8w58@kohls.com>
2018-08-31 11:13:41 -0700

    95. Unique Binary Search Trees II

--- 000UniqueBinarySearchTreesII.py
+++ 000UniqueBinarySearchTreesII.py
@@ -1,26 +1,47 @@
 # 95. Unique Binary Search Trees II
 
-# How to generate enough root node.
-# Note result = [] for every call of dfs.
+# Extend of 96. Unique Binary Search Trees 
 
-class Solution(object):
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
     def generateTrees(self, n):
         """
         :type n: int
         :rtype: List[TreeNode]
         """
         if n == 0:
-            return [[]]
-        return self.dfs(1, n+1)
-        
-    def dfs(self, start, end):
-        if start == end:
-            return None
+            return []
+
+        nums = [i for i in range(1, n + 1)]
+
+        return self.util(nums)
+
+    def util(self, nums):
+
+        if len(nums) == 0:
+            return [None]
+
+        if len(nums) == 1:
+            n = TreeNode(nums[0])
+            return [n]
+
+        # result = [treenode1, treenode2, ...]
         result = []
-        for i in xrange(start, end):
-            for l in self.dfs(start, i) or [None]:
-                for r in self.dfs(i+1, end) or [None]:
-                    node = TreeNode(i)
-                    node.left, node.right  = l, r
-                    result.append(node)
+        for i in range(len(nums)):
+            left = self.util(nums[:i])
+            right = self.util(nums[i + 1:])
+            for i_l in left:
+                for i_r in right:
+                    n = TreeNode(nums[i])
+                    n.left = i_l
+                    n.right = i_r
+                    result.append(n)
+
         return result
+            
