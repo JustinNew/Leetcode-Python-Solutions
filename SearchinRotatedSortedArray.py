5c0b68d6293d2459c18daa0d5d4de70cea2c90ce
Justin Tian <tkg8w58@kohls.com>
2018-09-05 13:40:40 -0700

    33. Search in Rotated Sorted Array

--- SearchinRotatedSortedArray.py
+++ SearchinRotatedSortedArray.py
@@ -1,5 +1,4 @@
 # 33. Search in Rotated Sorted Array
-# First find the rotated min point, then search in the two rotated array.
 
 class Solution:
     def search(self, nums, target):
@@ -9,46 +8,48 @@ class Solution:
         :rtype: int
         """
 
-        if len(nums) != 0:
-            pivot = self.pivotPoint(nums)
-        else:
+        if len(nums) == 0:
             return -1
 
-        if pivot == 0:
-            return self.binarySearch(nums, 0, len(nums) - 1, target)
-        elif target >= nums[0] and target <= nums[pivot - 1]:
-            return self.binarySearch(nums, 0, pivot - 1, target)
-        else:
-            return self.binarySearch(nums, pivot, len(nums) - 1, target)
+        def findPivot(arr):
 
-    def pivotPoint(self, nums):
+            l = 0
+            h = len(arr) - 1
 
-        left = 0
-        right = len(nums) - 1
-
-        while left <= right:
-            mid = int((left + right) / 2)
-            if nums[mid] < nums[right]:
-                right = mid
-            else:
-                left = mid + 1
-
-        return mid
-
-    def binarySearch(self, nums, left, right, target):
-
-            while left <= right:
-                mid = int((left + right) / 2)
-                if nums[mid] == target:
-                    return mid
-                elif nums[mid] > target:
-                    right = mid - 1
+            while l < h:
+                mid = int((l + h) / 2)
+                if arr[mid] > arr[h]:
+                    l = mid + 1
                 else:
-                    left = mid + 1
+                    h = mid
+
+            return l
+
+        def search(low, high, val):
+
+            if nums[low] > val or nums[high] < val:
+                return -1
+
+            l = low
+            h = high
+            while l <= h:
+                mid = int((l + h) / 2)
+                if nums[mid] == val:
+                    return mid
+                elif nums[mid] < val:
+                    l = mid + 1
+                else:
+                    h = mid - 1
 
             return -1
 
-if __name__ == '__main__':
+        m = findPivot(nums)
+        left = search(0, m - 1, target)
+        right = search(m, len(nums) - 1, target)
 
-    s = Solution()
-    print (s.search([0,1,2,3,4,5,6], 3))
+        if left != -1:
+            return left
+        elif right != -1:
+            return right
+        else:
+            return -1 
