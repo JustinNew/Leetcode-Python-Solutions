43b992f11a95af070fd1f227c961ba95bf94894e
Justin Tian <tkg8w58@kohls.com>
2018-09-09 14:28:50 -0700

    53. Maximum Subarray

--- 000MaximumSubarray.py
+++ 000MaximumSubarray.py
@@ -5,7 +5,15 @@
 # For example, given the array [-2,1,-3,4,-1,2,1,-5,4],
 # the contiguous subarray [4,-1,2,1] has the largest sum = 6.
 
-# Greedy
+##############################################################################################################
+# Dynamic programming
+# dp[i] = dp[i - 1] + nums[i] if dp[i - 1] > 0 else nums[i]
+# nums = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
+# dp =   [-1, 1, -2, 4,  3, 5, 6,  1, 5]
+
+# Optimized: 
+#    1. dp[i] -> curSum, we only need dp[i - 1] for dp[i].
+#    2. Use maxSum to reduce find the maximum of dp list. 
 
 class Solution(object):
     def maxSubArray(self, A):
