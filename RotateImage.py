0215f9484fe8cb116c754ec229c50620a79efb50
Justin Tian <tkg8w58@kohls.com>
2018-09-09 14:02:30 -0700

    48. Rotate Image

--- RotateImage.py
+++ RotateImage.py
@@ -33,3 +33,13 @@ class Solution:
             count -= 2
  
         return 
+
+'''
+/*
+ * clockwise rotate
+ * first reverse up to down, then swap the symmetry 
+ * 1 2 3     7 8 9     7 4 1
+ * 4 5 6  => 4 5 6  => 8 5 2
+ * 7 8 9     1 2 3     9 6 3
+*/
+'''
