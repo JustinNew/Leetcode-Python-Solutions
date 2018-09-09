9c5caa45a4c4b6c32d147a734b6b7199c037a72d
Justin Tian <tkg8w58@kohls.com>
2018-09-09 13:54:08 -0700

    48. Rotate Image

---
+++ RotateImage.py
@@ -0,0 +1,35 @@
+# 48. Rotate Image
+
+# Do it layer by layer.
+# Be careful about clockwise or counter clockwise.
+# Be careful about the indexes.
+
+class Solution:
+    def rotate(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: void Do not return anything, modify matrix in-place instead.
+        """
+       
+        if len(matrix) == 0:
+            return
+
+        if len(matrix) == 1:
+            return
+
+        count = len(matrix)
+        l = count - 1
+        i = 0
+        while count >= 2:
+            j = i
+            for k in range(count - 1):
+                temp = matrix[i][j]
+                matrix[i][j] = matrix[l - j][i]
+                matrix[l - j][i] = matrix[l - i][l - j]
+                matrix[l - i][l - j] = matrix[j][l - i]
+                matrix[j][l - i] = temp
+                j += 1
+            i += 1
+            count -= 2
+ 
+        return 
