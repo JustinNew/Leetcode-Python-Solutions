763fa3f0da86350f045ce8c51fcb9668b3d4cc5c
Justin Tian <tkg8w58@kohls.com>
2018-09-10 14:49:17 -0700

    73

--- 000SetMatrixZeroes.py
+++ 000SetMatrixZeroes.py
@@ -1,6 +1,9 @@
 # 73. Set Matrix Zeroes
 # Given a m x n matrix, if an element is 0, set its entire row and column to 0. Do it in place.
 
+# Use first row and first column as indicators.
+# Deal with first row and column separately.
+
 class Solution:
     def setZeroes(self, matrix):
         """
