2ca4ce074d7f6f4161a337e2c39d246e7b05b8d4
Justin Tian <tkg8w58@kohls.com>
2018-09-11 14:14:11 -0700

    91. Decode Ways

--- DecodeWays.py
+++ DecodeWays.py
@@ -21,8 +21,47 @@ class Solution:
 
             return dp[len(s)]
 
-if __name__ == '__main__':
+'''
+Dynamic Programming
+if s[i] != 0:
+    dp[i] += dp[i - 1]
 
-    s = Solution()
-    print (s.numDecodings('4757562545844617494555774581341211511296816786586787755257741178599337186486723247528324612117156948'))
-             
+if s[i - 2:i] >= 10 and s[i - 2:i] <= 26:
+    dp[i] += dp[i - 2]
+'''
+    ########################################################################################################################
+    # My solution.
+
+    def numDecodings(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+
+        l = len(s)
+        if s[0] == '0' or l == 0:
+            return 0
+        elif l == 1:
+            return 1
+
+        dp = [0 for i in range(l)]
+        
+        dp[0] = 1
+        if s[1] == '0':
+            if s[0] <= '2':
+                dp[1] = 1
+            else:
+                return 0
+        else:
+            if s[:2] >= '10' and s[:2] <= '26':
+                dp[1] = 2
+            else:
+                dp[1] = 1
+        
+        for i in range(2, l):
+            if s[i] != '0':
+                dp[i] += dp[i - 1]
+            if s[i - 1:i + 1] >= '10' and s[i - 1:i + 1] <= '26':
+                dp[i] += dp[i - 2]
+
+        return dp[-1]
