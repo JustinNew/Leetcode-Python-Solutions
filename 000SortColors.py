fec8078b4f47271073cecfca06ae76436f4b3cad
Justin Tian <tkg8w58@kohls.com>
2018-09-16 15:40:13 -0700

    75. Sort Colors

--- 000SortColors.py
+++ 000SortColors.py
@@ -8,7 +8,7 @@ class Solution:
         """
         count0 = 0
         count1 = 0
-        count2 = 2
+        count2 = 0
         
         for i in range(len(nums)):
             if nums[i] == 0:
@@ -35,3 +35,30 @@ class Solution:
             count2 -= 1
             
         return
+
+    '''
+    Requirement: one pass, inplace
+    The idea is to sweep all 0s to the left and all 2s to the right, then all 1s are left in the middle.
+    '''
+
+    def sortColors(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: void Do not return anything, modify nums in-place instead.
+        """
+
+        l = len(nums)
+        if l <= 1:
+            return
+
+        p0 = 0
+        p2 = l - 1
+        for i in range(l):
+            while nums[i] == 2 and i < p2:
+                nums[i], nums[p2] = nums[p2], nums[i]
+                p2 -= 1
+            while nums[i] == 0 and i > p0:
+                nums[i], nums[p0] = nums[p0], nums[i]
+                p0 += 1
+
+        return 
