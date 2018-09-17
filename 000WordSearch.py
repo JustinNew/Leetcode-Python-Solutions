2e562834a37c00e66701b324c98cc2b6423411a7
Justin Tian <tkg8w58@kohls.com>
2018-09-17 13:48:42 -0700

    79. Word Search

--- 000WordSearch.py
+++ 000WordSearch.py
@@ -6,7 +6,7 @@
 # The same letter cell may not be used more than once.
 
 # 'sequentially', so it is just single way without branch. 
-# DFS
+# DFS + BackTrack + help matrix
 
 class Solution:
     # @param board, a list of lists of 1 length string
@@ -38,3 +38,140 @@ class Solution:
         visited[(i, j)] = False
 
         return res
+
+    #####################################################################################################################################
+    # My own passed solution.
+    def exist(self, board, word):
+        """
+        :type board: List[List[str]]
+        :type word: str
+        :rtype: bool
+        """
+        if len(word) == 0:
+            return True
+        
+        m = len(board)
+        if m != 0:
+            n = len(board[0])
+        else:
+            return False
+
+        used = [[0 for i in range(n)] for j in range(m)]
+        flag = 0
+
+        for i in range(m):
+            for j in range(n):
+                if board[i][j] == word[0]:
+                    used[i][j] = 1
+                    if self.dfs(i, j, 1, board, word, used):
+                        flag = 1
+                    used[i][j] = 0
+                if flag == 1:
+                    return True
+
+        return False
+
+    def dfs(self, i, j, index, board, word, used):
+
+        if index == len(word):
+            return True
+
+        if i + 1 < len(board) and board[i + 1][j] == word[index] and used[i + 1][j] == 0:
+            used[i + 1][j] = 1
+            if self.dfs(i + 1, j, index + 1, board, word, used):
+                return True
+            used[i + 1][j] = 0
+
+        if i - 1 >= 0 and board[i - 1][j] == word[index] and used[i - 1][j] == 0:
+            used[i - 1][j] = 1
+            if self.dfs(i - 1, j, index + 1, board, word, used):
+                return True
+            used[i - 1][j] = 0
+
+        if j + 1 < len(board[0]) and board[i][j + 1] == word[index] and used[i][j + 1] == 0:
+            used[i][j + 1] = 1
+            if self.dfs(i, j + 1, index + 1, board, word, used):
+                return True
+            used[i][j + 1] = 0
+
+        if j - 1 >= 0 and board[i][j - 1] == word[index] and used[i][j - 1] == 0:
+            used[i][j - 1] = 1
+            if self.dfs(i, j - 1, index + 1, board, word, used):
+                return True
+            used[i][j - 1] = 0
+
+        return False
+
+    #####################################################################################################################################
+    # Time Limit Exceeded.
+    def exist(self, board, word):
+        """
+        :type board: List[List[str]]
+        :type word: str
+        :rtype: bool
+        """
+
+        if len(board) == 0 and len(word) == 0:
+            return True
+        elif len(board) == 0 and len(word) != 0:
+            return False
+
+        m = len(board)
+        n = len(board[0])
+
+        visited = [[0 for i in range(n)] for j in range(m)]
+        
+        def search(i, j, k):
+            if visited[i][j] == 0 and board[i][j] == word[k]:
+                if k == len(word) - 1:
+                    return True
+
+                visited[i][j] = 1
+                up = search(i - 1, j, k + 1) if i - 1 >= 0 else False
+                down = search(i + 1, j, k + 1) if i + 1 < m else False
+                left = search(i, j - 1, k + 1) if j - 1 >= 0 else False
+                right = search(i, j + 1, k + 1) if j + 1 < n else False
+                visited[i][j] = 0
+
+                return up or down or left or right
+            else:
+                return False
+
+        for i in range(m):
+            for j in range(n):
+                if search(i, j, 0):
+                    return True
+
+        return False
+
+    # Passed after change.
+    def exist(self, board, word):
+        if len(board) == 0 and len(word) == 0:
+            return True
+        elif len(board) == 0 and len(word) != 0:
+            return False
+
+        m = len(board)
+        n = len(board[0])
+
+        visited = [[0 for i in range(n)] for j in range(m)]
+
+        def search(i, j, k):
+            if k == len(word):
+                return True
+            
+            if i < 0 or i == m or j < 0 or j == n or visited[i][j] == 1 or board[i][j] != word[k]:
+                return False
+
+            visited[i][j] = 1
+            res = search(i - 1, j, k + 1) or search(i + 1, j, k + 1) or search(i, j - 1, k + 1) or search(i, j + 1, k + 1)
+            visited[i][j] = 0
+            
+            return res
+
+        for i in range(m):
+            for j in range(n):
+                if search(i, j, 0):
+                    return True
+
+        return False
