0801660a005b291d9b2decd1170d989ff75b44a9
Justin Tian <tkg8w58@kohls.com>
2018-09-20 14:22:31 -0700

    127. Word Ladder

--- 000WordLadder.py
+++ 000WordLadder.py
@@ -1,7 +1,9 @@
 # 127. Word Ladder
 
-class Solution(object):
-    # Time Limit Exceeded.
+class Solution:
+
+    # BFS: level order traversal
+    # Time Limit Exceeded
     def ladderLength(self, beginWord, endWord, wordList):
         """
         :type beginWord: str
@@ -13,33 +15,30 @@ class Solution(object):
         if endWord not in wordList:
             return 0
 
-        result = 1
-        used = [0 for i in range(len(wordList))]
-
-        def getDiff(word1, word2):
-            diff = 0
-            for k in range(len(word1)):
-                if word1[k] != word2[k]:
-                    diff += 1
-            return diff
-
+        count = 1
         candidate = [beginWord]
+        d = {w:0 for w in wordList}
+
         while 1:
+
+            count += 1
             temp = []
             for w in candidate:
-                for i in range(len(wordList)):
-                    if used[i] == 0:
-                        if getDiff(wordList[i], w) == 1:
-                            temp.append(wordList[i])
-                            used[i] = 1
-            result += 1
-            if len(temp) == 0:
+                for i in range(len(w)):
+                    for s in 'abcdefghijklmnopqrstuvwxyz':
+                        t = w[:i] + s + w[i + 1:]
+                        if t == endWord:
+                            return count
+                        elif t in wordList and d[t] == 0:
+                            temp.append(t)
+                            d[t] = 1
+
+            if not temp:
                 return 0
-            if endWord in temp:
-                return result
+
             candidate = temp
 
-    # Time Limit Exceeded.
+    # Use set and passed.
     def ladderLength(self, beginWord, endWord, wordList):
         """
         :type beginWord: str
@@ -47,24 +46,51 @@ class Solution(object):
         :type wordList: List[str]
         :rtype: int
         """
+        wordList = set(wordList)
         if endWord not in wordList:
             return 0
 
-        result = 1
-        used = [0 for i in range(len(wordList))]
-
+        count = 1
         candidate = [beginWord]
+
         while 1:
+
+            count += 1
             temp = []
             for w in candidate:
-                for i in range(len(wordList)):
-                    if used[i] == 0:
-                        if sum([1 for k, l in zip(w, wordList[i]) if k != l]) == 1:
-                            temp.append(wordList[i])
-                            used[i] = 1
-            result += 1
-            if len(temp) == 0:
+                for i in range(len(w)):
+                    for s in 'abcdefghijklmnopqrstuvwxyz':
+                        t = w[:i] + s + w[i + 1:]
+                        if t == endWord:
+                            return count
+                        elif t in wordList:
+                            temp.append(t)
+                            wordList.remove(t)
+
+            if not temp:
                 return 0
-            if endWord in temp:
-                return result
+
             candidate = temp
+
+
+    # From Discussion, use set.
+    def ladderLength(self, beginWord, endWord, wordList):
+        """
+        :type beginWord: str
+        :type endWord: str
+        :type wordList: List[str]
+        :rtype: int
+        """
+        wordList = set(wordList)
+        queue = collections.deque([[beginWord, 1]])
+        while queue:
+            word, length = queue.popleft()
+            if word == endWord:
+                return length
+            for i in range(len(word)):
+                for c in 'abcdefghijklmnopqrstuvwxyz':
+                    next_word = word[:i] + c + word[i+1:]
+                    if next_word in wordList:
+                        wordList.remove(next_word)
+                        queue.append([next_word, length + 1])
+        return 0
