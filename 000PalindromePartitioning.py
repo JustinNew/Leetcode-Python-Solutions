2351e454c27a2d098c2144644891089af85f7b6b
Justin Tian <tkg8w58@kohls.com>
2018-09-28 14:12:50 -0700

    131. Palindrome Partitioning

--- 000PalindromePartitioning.py
+++ 000PalindromePartitioning.py
@@ -1,6 +1,6 @@
 # 131. Palindrome Partitioning
 
-# Intuition recursive solution
+# 1. Recursion solution
 # Some similarity to 95. Unique Binary Search Trees II
 
 class Solution:
@@ -23,3 +23,36 @@ class Solution:
                     result.append([s[:i]] + l)
 
         return result
+
+'''
+2. Dynamic Programming
+   dp[n] = dp[n - 1] + [s[n]] 
+         + dp[n - 2] + [s[n - 1, n]] if s[n - 1, n] is palindrome
+         + dp[n - 3] + [s[n - 2, n - 1, n]] if s[n - 2, n - 1, n] is palindrome 
+         ...
+         + dp[1] + [s[1:]] if s[1:] is palindrome
+'''
+
+    def partition(self, s):
+        """
+        :type s: str
+        :rtype: List[List[str]]
+        """
+
+        if len(s) == 0:
+            return []
+
+        dp = [[] for i in range(len(s))]
+        dp[0] = [[s[0]]]
+
+        for i in range(1, len(s)):
+            for j in range(i):
+                t = s[i - j:i + 1]
+                if t == t[::-1]:
+                    for l in dp[i - j - 1]: 
+                        dp[i].append(l + [t])
+            t = s[:i + 1]
+            if t == t[::-1]:
+                dp[i].append([t]) 
+
+        return dp[-1]       
