43f3d3b60c48087d2b990cb634fdc6c5721f725a
Justin Tian <tkg8w58@kohls.com>
2018-09-29 20:07:09 -0700

    138. Copy List with Random Pointer

--- CopyListwithRandomPointer.py
+++ CopyListwithRandomPointer.py
@@ -1,5 +1,11 @@
 # 138. Copy List with Random Pointer
 
+'''
+Similar to copy linked list.
+Need to have two lists to record the nodes of the original and the copied nodes in order as we copy the list.
+'''
+
+
 # Definition for singly-linked list with a random pointer.
 # class RandomListNode(object):
 #     def __init__(self, x):
@@ -17,38 +23,36 @@ class Solution(object):
         if not head:
             return None
 
-        toCopyNodes = []
-        CopiedNodes = []
-        dummy = RandomListNode(0)
-        head_copied = dummy
+        origin = []
+        copied = []
 
-        toCopyNodes.append(head)
-        n = RandomListNode(head.label)
-        CopiedNodes.append(n)
-        dummy.next = n
-        dummy = dummy.next
+        copy = RandomListNode(head.label)
+        res = copy
+
+        origin.append(head)
+        copied.append(copy)
 
         while head:
+            next = head.next
+            random = head.random
 
-            if head.next and head.next not in toCopyNodes:
-                toCopyNodes.append(head.next)
-                n_next = RandomListNode(head.next.label)
-                CopiedNodes.append(n_next)
-                dummy.next = n_next
-            elif head.next:
-                i = toCopyNodes.index(head.next)
-                dummy.next = CopiedNodes[i]
+            if next in origin:
+                i = origin.index(next)
+                copy.next = copied[i]
+            elif next:
+                copy.next = RandomListNode(next.label)
+                origin.append(next)
+                copied.append(copy.next)
 
-            if head.random and head.random not in toCopyNodes:
-                toCopyNodes.append(head.random)
-                n_random = RandomListNode(head.random.label)
-                CopiedNodes.append(n_random)
-                dummy.random = n_random
-            elif head.random:
-                i = toCopyNodes.index(head.random)
-                dummy.random = CopiedNodes[i]
+            if random in origin:
+                i = origin.index(random)
+                copy.random = copied[i]
+            elif random:
+                copy.random = RandomListNode(random.label)
+                origin.append(random)
+                copied.append(copy.random)
 
-            head = head.next
-            dummy = dummy.next
+            head = next
+            copy = copy.next
 
-        return head_copied.next
+        return res
