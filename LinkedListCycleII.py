a1036067cddaf28dd05f09c88eed51b471110108
Justin Tian <tkg8w58@kohls.com>
2018-10-03 13:20:50 -0700

    142. Linked List Cycle II

--- LinkedListCycleII.py
+++ LinkedListCycleII.py
@@ -1,6 +1,4 @@
-# 142. Linked List Cycle II
-
-# Tricky: check slow.next, fast.next and fast.next.next is not None.
+# 141. Linked List Cycle
 
 # Definition for singly-linked list.
 # class ListNode(object):
@@ -9,31 +7,63 @@
 #         self.next = None
 
 class Solution(object):
+    def hasCycle(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+
+        if not head:
+            return False
+
+        slow = head
+        fast = head
+
+        while slow.next and fast.next and fast.next.next:
+            slow = slow.next
+            fast = fast.next.next
+
+            if slow == fast:
+                return True
+
+        return False
+
+######################################################################################################################
+# 142. Linked List Cycle II
+
+'''
+Tricky: check slow.next, fast.next and fast.next.next is not None.
+First slow and fast, then two pointers one from head and the other from meet point.
+'''
+
+class Solution(object):
     def detectCycle(self, head):
         """
         :type head: ListNode
         :rtype: ListNode
         """
+
         if not head:
             return None
 
-        fast = head
         slow = head
+        fast = head
 
+        flag = 0
         while slow.next and fast.next and fast.next.next:
-
             slow = slow.next
             fast = fast.next.next
 
             if slow == fast:
+                flag = 1
                 break
-            
-        if not slow.next or not fast.next or not fast.next.next:
-            return None
-        
-        slow = head
-        while slow != fast:
-            slow = slow.next
-            fast = fast.next
 
-        return slow
+        if flag == 1:
+            p1 = head
+            p2 = slow
+            while p1 != p2:
+                p1 = p1.next
+                p2 = p2.next
+            return p1
+        else:
+            return None
