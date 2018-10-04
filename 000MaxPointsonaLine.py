ab3cd1c5cc8bf984744646a5fdc0f519c2a7fea4
Justin Tian <tkg8w58@kohls.com>
2018-10-04 13:44:12 -0700

    149. Max Points on a Line

---
+++ 000MaxPointsonaLine.py
@@ -0,0 +1,16 @@
+# 149. Max Points on a Line
+
+# Definition for a point.
+# class Point(object):
+#     def __init__(self, a=0, b=0):
+#         self.x = a
+#         self.y = b
+
+class Solution(object):
+    def maxPoints(self, points):
+        """
+        :type points: List[Point]
+        :rtype: int
+        """
+
+        
