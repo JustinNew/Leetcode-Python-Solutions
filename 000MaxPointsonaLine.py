0a349fe41c8ed9b45eedff60c48486122b58710e
Justin Tian <tkg8w58@kohls.com>
2018-10-05 14:14:32 -0700

    149. Max Points on a Line

--- 000MaxPointsonaLine.py
+++ 000MaxPointsonaLine.py
@@ -6,6 +6,12 @@
 #         self.x = a
 #         self.y = b
 
+'''
+1. Consider all possible lines with 1st point, then discard 1st point.
+2. Consider all possible lines with 2nd point, then discard 2nd point.
+3. ...
+'''
+
 class Solution(object):
     def maxPoints(self, points):
         """
@@ -13,4 +19,19 @@ class Solution(object):
         :rtype: int
         """
 
-        
+        l = len(points)
+        m = 0
+        for i in range(l):
+            dic = {'i': 1}
+            same = 0
+            for j in range(i+1, l):
+                tx, ty = points[j].x, points[j].y
+                if tx == points[i].x and ty == points[i].y: 
+                    same += 1
+                    continue
+                if points[i].x == tx: slope = 'i'
+                else:slope = (points[i].y-ty) * 1.0 /(points[i].x-tx)
+                if slope not in dic: dic[slope] = 1
+                dic[slope] += 1
+            m = max(m, max(dic.values()) + same)
+    return m
