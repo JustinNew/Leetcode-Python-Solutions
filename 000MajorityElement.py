da643688458b3741dc7c6dd1e3f200290a6e2376
Justin Tian <tkg8w58@kohls.com>
2018-10-09 20:18:49 -0700

    169. Majority Element

--- 000MajorityElement.py
+++ 000MajorityElement.py
@@ -5,21 +5,6 @@
 # You may assume that the array is non-empty and the majority element always exist in the array.
 
 class Solution(object):
-    #
-    # Boyer–Moore majority vote algorithm
-    #
-    def majorityElement_moore(self, nums):
-        majority_num = 0
-        count = 0
-        for num in nums:
-            if count == 0:
-                majority_num = num
-            if majority_num != num:
-                count -= 1
-            else:
-                count += 1
-        return majority_num
-
     def majorityElement(self, nums):
         """
         :type nums: List[int]
@@ -40,39 +25,82 @@ class Solution(object):
         l = len(nums)
         for i in c.keys():
             if c.get(i, 0) > l/2.0:
-                return i
+
+#######################################################################################################
+# 169. Majority Element
+
+class Solution:
+    #
+    # Boyer–Moore majority vote algorithm
+    #
 
     def majorityElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        count = 0
+        cur = float('inf')
 
-        nums = sorted(nums)
-        return nums[len(nums)/2]
+        for i in nums:
+            if cur == i:
+                count += 1
+            else:
+                if count == 0:
+                    cur = i
+                    count += 1
+                else:
+                    count -= 1
 
+        return cur
 
+#######################################################################################################
+# Exactly the same idea as 169.
 # 229. Majority Element II
-# Boyer-Moore Majority Vote algorithm 
+
+# Boyer-Moore Majority Vote algorithm
 # https://gregable.com/2013/10/majority-vote-algorithm-find-majority.html
 
 class Solution:
-
-    # @param {integer[]} nums
-    # @return {integer[]}
     def majorityElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+       
+        cur1 = float('inf')
+        cur2 = float('inf')
+        count1 = 0
+        count2 = 0
 
-        if not nums:
-            return []
-
-        count1, count2, candidate1, candidate2 = 0, 0, 0, 1
-        for n in nums:
-            if n == candidate1:
+        for i in nums:
+            if cur1 == i:
                 count1 += 1
-            elif n == candidate2:
+            elif cur2 == i:
                 count2 += 1
-            elif count1 == 0:
-                candidate1, count1 = n, 1
-            elif count2 == 0:
-                candidate2, count2 = n, 1
             else:
-                count1, count2 = count1 - 1, count2 - 1 # Note: both need to subtract 1
+                if count1 == 0:
+                    cur1 = i
+                    count1 += 1
+                elif count2 == 0:
+                    cur2 = i
+                    count2 += 1
+                else:
+                    count1 -= 1
+                    count2 -= 1
 
-        return [n for n in (candidate1, candidate2)
-                        if nums.count(n) > len(nums) // 3]
+        l = len(nums)
+        count1 = 0
+        count2 = 0
+        for i in nums:
+            if i == cur1:
+                count1 += 1
+            elif i == cur2:
+                count2 += 1
+        res = []
+        if count1 > int(l / 3):
+            res.append(cur1)
+        if count2 > int(l / 3):
+            res.append(cur2)
+
+        return res
