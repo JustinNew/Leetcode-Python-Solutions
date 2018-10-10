6bc093220bb3a2b4b65f6c29dd4a8dd1acd2e8e7
Justin Tian <tkg8w58@kohls.com>
2018-10-10 14:19:43 -0700

    179. Largest Number

--- 000LargestNumber.py
+++ 000LargestNumber.py
@@ -8,8 +8,10 @@ class Solution:
     def largestNumber(self, nums):
 
         nums = [str(x) for x in nums]
+        
         # Discarded in Python 3.x.
         # nums.sort(cmp=lambda x, y: cmp(y+x, x+y))
+        
         from functools import cmp_to_key
         nums.sort(key=cmp_to_key(lambda x, y: -1 if str(x) + str(y) > str(y) + str(x) else 1))
         return ''.join(nums).lstrip('0') or '0'
