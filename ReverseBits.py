ff37b35321e846be338c748d2dc4bbdf7a9afa53
Justin Tian <tkg8w58@kohls.com>
2018-10-10 20:24:05 -0700

    190. Reverse Bits

--- ReverseBits.py
+++ ReverseBits.py
@@ -19,3 +19,23 @@ class Solution:
             result = result * 2
 
         return result
+
+    def reverseBits(self, n):
+        
+        res = []
+        while n > 0:
+            t = n % 2
+            res.append(t)
+            n = int(n / 2)
+            
+        l = len(res)
+        if l < 32:
+            for i in range(32 - l):
+                res.append(0)
+                
+        s = 0
+        res.reverse()
+        for i in range(32):
+            s += res[i] * 2 ** i
+            
+        return s
