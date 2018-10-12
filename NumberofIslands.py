ed388c3f75d09c3256f40b777320b448782b2cb8
Justin Tian <tkg8w58@kohls.com>
2018-10-12 14:01:47 -0700

    200. Number of Islands

--- NumberofIslands.py
+++ NumberofIslands.py
@@ -2,7 +2,7 @@
 # Facebook Tag
 
 # Go through each element
-# Do a DFS
+# Do a BFS
 # Create a visited matrix
 
 class Solution(object):
@@ -48,3 +48,88 @@ class Solution(object):
             self.util(i, j+1, m, n, grid, visited)
 
         return
+
+    #############################################################################################################################
+    # Maximum recursion depth exceeded in comparison
+
+    def numIslands(self, grid):
+        """
+        :type grid: List[List[str]]
+        :rtype: int
+        """
+
+        m = len(grid)
+        if m == 0:
+            return 0
+
+        n = len(grid[0])
+
+        helper = [[0 for i in range(n)] for j in range(m)]
+        count = 0
+
+        def bfs(i, j):
+            if i < 0 or i >= m or j < 0 or j >= n:
+                return
+
+            if helper[i][j] == -1:
+                return
+            elif helper[i][j] == 1:
+                return 
+            else:
+                helper[i][j] = -1
+                bfs(i - 1, j)
+                bfs(i + 1, j)
+                bfs(i, j - 1)
+                bfs(i, j + 1)
+                helper[i][j] = 1
+
+                return
+
+        for k in range(m):
+            for l in range(n):
+                if grid[k][l] == "1" and helper[k][l] == 0:
+                    count += 1
+                    bfs(k, l)
+
+        return count
+
+
+    # The BFS is not optimzed in the previous one.
+    def numIslands(self, grid):
+        """
+        :type grid: List[List[str]]
+        :rtype: int
+        """
+        m = len(grid)
+        if m == 0:
+            return 0
+
+        n = len(grid[0])
+
+        helper = [[0 for i in range(n)] for j in range(m)]
+        count = 0
+
+        def bfs(i, j):
+            
+            helper[i][j] = -1
+                
+            if i - 1 >= 0 and grid[i - 1][j] == "1" and helper[i - 1][j] == 0:
+                bfs(i - 1, j)
+            if i + 1 < m and grid[i + 1][j] == "1" and helper[i + 1][j] == 0:
+                bfs(i + 1, j)
+            if j - 1 >= 0 and grid[i][j - 1] == "1" and helper[i][j - 1] == 0:
+                bfs(i, j - 1)
+            if j + 1 < n and grid[i][j + 1] == "1" and helper[i][j + 1] == 0:    
+                bfs(i, j + 1)
+                    
+            helper[i][j] = 1
+
+            return
+
+        for k in range(m):
+            for l in range(n):
+                if grid[k][l] == "1" and helper[k][l] == 0:
+                    count += 1
+                    bfs(k, l)
+
+        return count
