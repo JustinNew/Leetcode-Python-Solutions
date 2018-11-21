6b65beac9add9ea396c905cbdb3c9ecebac3ee2a
Justin Tian <tkg8w58@kohls.com>
2018-11-21 15:52:24 -0800

    33. Search in Rotated Sorted Array

--- SearchinRotatedSortedArray.py
+++ SearchinRotatedSortedArray.py
@@ -1,5 +1,28 @@
 # 33. Search in Rotated Sorted Array
 
+# Facebook Tag
+
+'''
+Suppose an array sorted in ascending order is rotated at some pivot unknown to you beforehand.
+
+(i.e., [0,1,2,4,5,6,7] might become [4,5,6,7,0,1,2]).
+
+You are given a target value to search. If found in the array return its index, otherwise return -1.
+
+You may assume no duplicate exists in the array.
+
+Your algorithm's runtime complexity must be in the order of O(log n).
+
+Example 1:
+
+Input: nums = [4,5,6,7,0,1,2], target = 0
+Output: 4
+Example 2:
+
+Input: nums = [4,5,6,7,0,1,2], target = 3
+Output: -1
+'''
+
 class Solution:
     def search(self, nums, target):
         """
@@ -8,48 +31,75 @@ class Solution:
         :rtype: int
         """
 
-        if len(nums) == 0:
-            return -1
-
         def findPivot(arr):
+            l = len(arr)
+            if l == 1:
+                return 0
 
-            l = 0
-            h = len(arr) - 1
-
-            while l < h:
-                mid = int((l + h) / 2)
-                if arr[mid] > arr[h]:
-                    l = mid + 1
+            low = 0
+            high = l - 1
+            while low < high:
+                mid = int((low + high) / 2)
+                if arr[mid] > arr[high]:
+                    low = mid + 1
                 else:
-                    h = mid
+                    high = mid
 
-            return l
+            return low
 
-        def search(low, high, val):
+        def findTarget(low, high):
 
-            if nums[low] > val or nums[high] < val:
-                return -1
-
-            l = low
-            h = high
-            while l <= h:
-                mid = int((l + h) / 2)
-                if nums[mid] == val:
+            while low <= high:
+                mid = int((low + high) / 2)
+                if nums[mid] == target:
                     return mid
-                elif nums[mid] < val:
-                    l = mid + 1
+                elif nums[mid] > target:
+                    high = mid - 1
                 else:
-                    h = mid - 1
+                    low = mid + 1
 
             return -1
 
-        m = findPivot(nums)
-        left = search(0, m - 1, target)
-        right = search(m, len(nums) - 1, target)
+        p = findPivot(nums)
+        i1 = findTarget(0, p - 1)
+        i2 = findTarget(p, len(nums) - 1)
 
-        if left != -1:
-            return left
-        elif right != -1:
-            return right
+        if i1 != -1:
+            return i1
+        elif i2 != -1:
+            return i2
         else:
-            return -1 
+            return -1
+
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+
+        l = len(nums)
+        if l == 0:
+            return -1
+
+        low = 0
+        high = l - 1
+        while low <= high:
+            mid = int((low + high) / 2)
+            if nums[mid] == target:
+                return mid
+            else:
+                # Pivot on the right
+                if nums[mid] > nums[high]:
+                    if nums[low] <= target and target <= nums[mid]:
+                        high = mid - 1
+                    else:
+                        low = mid + 1
+                # Pivot on the left
+                else:
+                    if nums[mid] <= target and target <= nums[high]:
+                        low = mid + 1
+                    else:
+                        high = mid - 1
+
+        return -1
